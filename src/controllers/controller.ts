b26a5fcae49a5d58c828763e1d90021258782b53
Emanuele <s1116727@studenti.univpm.it>
2024-07-04 23:45:31 +0200

    queue manager

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,5 +1,18 @@
 import { getAllUser } from "../models/users";
+import HttpStatusCode from "../utils/status_code"
+import { Request, Response } from 'express';
+import { myQueue } from '../singleton/queueManager';
 
 export async function getAllDatasets(req: any, res: any) {
-      return res.status(200).json(await getAllUser());
+      return res.status(HttpStatusCode.OK).json(await getAllUser());
 }
+
+export const addJob = async (req: Request, res: Response) => {
+  try {
+    const jobData = req.body;
+    await myQueue.add('my-job', jobData);
+    res.status(200).json({ message: 'Job added to queue' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to add job to queue', error });
+  }
+};
